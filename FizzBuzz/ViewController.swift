7f6e7462a83fc9a4ca4b58c8e2a5f01c8b00caa0
Sakina Ali <sali2@sakinas-macbook-air.local>
2017-06-13 09:58:01 -0500

    Initial Commit

--- FizzBuzz/ViewController.swift
+++ FizzBuzz/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var myNumberLabel: UILabel!
+    var number = 0
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       myNumberLabel.text = "\(number)"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func screenTapped(_ sender: UITapGestureRecognizer)
+    {
+        print("Screen Tapped")
+        number += 1
+    if number % 15 == 0
+        {
+            print("FizzBuzz")
+            view.backgroundColor = UIColor.yellow
+            myNumberLabel.text = "FizzBuzz"
+            myNumberLabel.textColor = UIColor.black
+            myNumberLabel.font = myNumberLabel.font.withSize(50)
+        }
+    else  if number % 3 == 0
+        {
+            print("Fizz")
+            view.backgroundColor = UIColor.red
+            myNumberLabel.text = "Fizz"
+            myNumberLabel.textColor = UIColor.white
+            myNumberLabel.font = myNumberLabel.font.withSize(100)
     }
-
-
+    else if number % 5 == 0
+        {
+            print("Buzz")
+            view.backgroundColor = UIColor.blue
+            myNumberLabel.text = "Buzz"
+            myNumberLabel.textColor = UIColor.white
+            myNumberLabel.font = myNumberLabel.font.withSize(100)
+        }
+    else
+        {
+        myNumberLabel.text = "\(number)"
+        view.backgroundColor = UIColor.black
+        myNumberLabel.font = myNumberLabel.font.withSize(100)
+        }
+        
+    }
+    @IBAction func longPress(_ sender: Any)
+    {
+        var number = 0
+        myNumberLabel.text = "\(number)"
+        view.backgroundColor = UIColor.black
+         myNumberLabel.font = myNumberLabel.font.withSize(100)
+        myNumberLabel.textColor = UIColor.white
+        
+    }
+    
+    
+    
 }
 
